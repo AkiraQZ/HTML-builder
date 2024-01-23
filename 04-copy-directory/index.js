d8b8aa2310d5f41317373aab9cdf4170f2f02708
AkiraQZ <vboyrkin@gmail.com>
2024-01-23 15:53:16 +0800

    feat: 04-copy-directory complited

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function copyDir(source, destination) {
+  try {
+    try {
+      await fs.access(destination);
+    } catch (err) {
+      await fs.mkdir(destination);
+    }
+    const files = await fs.readdir(source);
+    for (const file of files) {
+      const current = await fs.lstat(path.join(source, file));
+      if (current.isDirectory()) {
+        await copyDir(path.join(source, file), path.join(destination, file));
+      } else {
+        await fs.copyFile(
+          path.join(source, file),
+          path.join(destination, file),
+        );
+      }
+    }
+  } catch (err) {
+    console.error('Error copying directory: ', err);
+  }
+}
+
+copyDir('04-copy-directory/files', '04-copy-directory/files-copy');
